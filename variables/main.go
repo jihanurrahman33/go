422dbb71842b8860d91d2f44b4b7041c906e33a1
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-04-08 23:35:57 +0600

    variable in go

--- variables/main.go
+++ variables/main.go
@@ -1,21 +1,49 @@
 package main
 
 import "fmt"
+const LoginToken string ="Nishak"//public variable
+//making the first letter capital means im making it public
 
 func main() {
 	var username string = "Nishak"
 	fmt.Println(username)
 	//  %T represent type of the variable
-	fmt.Printf("Variable is of type: %T ",username)
+	fmt.Printf("Variable is of type: %T\n ",username)
 
 
 
 	var isLoggedIn bool = false
 	fmt.Println(isLoggedIn)
 	//  %T represent type of the variable
-	fmt.Printf("Variable is of type: %T ",isLoggedIn)
+	fmt.Printf("Variable is of type: %T\n ",isLoggedIn)
 
-	var smallValue int =10
+	var smallValue int =1000000000000000000
 	//  %V represent the placeholder of the variable value
-	fmt.Printf("Value is: %v",smallValue)
+	fmt.Printf("Value is: %v\n",smallValue)
+
+	var secondSmallValue uint8 =255
+	//  %T represent the type of variable 
+	fmt.Printf("Value is: %T\n",secondSmallValue)
+
+
+	var smallFloatValue float32 =255.32
+	//  %T represent the type of variable 
+	fmt.Printf("Value is: %T\n",smallFloatValue)
+
+
+	//default value
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+
+//implicit type
+	var website="google.com"
+	fmt.Println(website)
+	
+	//no var style
+	numberOfUser:=30000
+	fmt.Println(numberOfUser)
+
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Value is: %T\n",LoginToken)
 }
